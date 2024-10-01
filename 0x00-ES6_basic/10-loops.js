5d4322713edd69123a1cc9b7e4f11860538d495b
wandilemawelela <wandile1997@gmail.com>
2024-10-01 01:27:31 +0000

    [ADD] Completed task 10: Rewrite the function appendToEachArrayValue to use ES6’s for...of operator. And don’t forget that var is not ES6-friendly.

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,10 @@
+export default function appendToEachArrayValue(array, appendString) {
+  for (let idx in array) {
+    if (array.hasOwnProperty(idx)) {
+      array[idx] = appendString + array[idx];
+    }
+  }
+
+  return array;
+}
+
