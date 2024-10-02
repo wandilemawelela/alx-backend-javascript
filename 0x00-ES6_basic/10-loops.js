fc00e551dac8db59967e7bf4621ee15292925c9e
wandilemawelela <wandile1997@gmail.com>
2024-10-02 17:23:53 +0000

    [FIX] Eslint

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,10 +1,3 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (let idx in array) {
-    if (array.hasOwnProperty(idx)) {
-      array[idx] = appendString + array[idx];
-    }
-  }
-
-  return array;
+  return array.map((value) => appendString + value);
 }
-
