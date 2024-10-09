6994c57a05cc89121fe37bbbc2f1bcf8360c5f66
wandilemawelela <wandile1997@gmail.com>
2024-10-09 18:38:28 +0000

    [ADD] Completed task 2: Write an interface named Directors that extends Teacher. It requires an attribute named numberOfReports(number).

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -7,6 +7,10 @@ interface Teacher {
   [key: string]: any;
 }
 
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
 const teacher1: Teacher = {
   firstName: 'John',
   lastName: 'Doe',
@@ -24,5 +28,14 @@ const teacher2: Teacher = {
   contract: true,
 };
 
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
 console.log(teacher1);
 console.log(teacher2);
+console.log(director1);
