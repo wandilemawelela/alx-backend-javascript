8ea27af190f0f67abf0605d5afd4a6e406d7de1a
wandilemawelela <wandile1997@gmail.com>
2024-11-28 19:59:43 +0000

    [FIX] Semistandard

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -5,15 +5,15 @@ const sendPaymentRequestToApi = require('./3-payment');
 
 describe('sendPaymentRequestToApi', function () {
   let spy;
-  
+
   beforeEach(function () {
     spy = sinon.spy(Utils, 'calculateNumber');
   });
-  
+
   afterEach(function () {
     spy.restore();
   });
-  
+
   it('should call Utils.calculateNumber with SUM, 100, and 20', function () {
     sendPaymentRequestToApi(100, 20);
     expect(spy.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
